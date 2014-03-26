b7fde497c31259c1e59177fb00b6d3cdfa02424a
[Mohammed Amin] <[amnmhmd@gmail.com]>
2014-03-25 20:59:51 -0400

    strings.rb configuration

---
+++ strings.rb
@@ -0,0 +1,36 @@
+def capital(string)
+	string.capitalize
+end
+
+def length(string)
+	string.length
+end
+
+def down(string)
+	string.downcase!
+end
+
+def tempfix(string)
+	string.gsub("*", "")
+end
+
+def permfix(string)
+	string.gsub!("*", "")
+end
+
+def rever(string)
+	string.reverse
+end
+
+def strp(string)
+	string.strip
+end
+
+def sym(string)
+	string.to_sym
+end
+
+def upletter(string)
+	string.upcase
+end
+
